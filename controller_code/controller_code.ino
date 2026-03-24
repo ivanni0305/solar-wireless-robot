2155b44932ceba2145876cb6743944a70037d90f
ivanni0305 <ivanni0305@gmail.com>
2026-03-23 23:36:34 -0400

    Add files via upload

---
+++ controller_code/controller_code.ino
@@ -0,0 +1,36 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+uint8_t robotMAC[] = {0xAF, 0xf0, 0x0F, 0x68, 0xAA, 0x00};
+
+#define VRX 34
+#define VRY 35
+
+typedef struct {
+  int x;
+  int y;
+} JoystickData;
+
+JoystickData data;
+esp_now_peer_info_t peerInfo;
+
+void onSent(const uint8_t *mac_addr, esp_now_send_status_t status) {}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  esp_now_register_send_cb(onSent);
+  memcpy(peerInfo.peer_addr, robotMAC, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+  Serial.println("Controller ready");
+}
+
+void loop() {
+  data.x = analogRead(VRX);
+  data.y = analogRead(VRY);
+  esp_now_send(robotMAC, (uint8_t *)&data, sizeof(data));
+  delay(20);
+}
